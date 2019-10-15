b7be3d7d479df13d7768688ef92bcd2b957bea9b
gad26032 <gad26032@gmail.com>
2019-10-15 14:38:29 +0300

    review

---
+++ typing_example.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from typing import List
+
+
+def f1(url: str) -> pd.DataFrame:
+    lllllll = pd.DataFrame()
+
+    return lllllll
+
+
+b = []
+
+f1(b)
+
+
+
+def f2(url_list: List[str]) -> pd.DataFrame:
+    """
+
+    :param url_list:
+    :return:
+    """
+    lllllll = pd.DataFrame()
+
+    return lllllll
+
+
+
+c: List[int] = [1,2,3,4]
+
+
+f2(['1', '23'])
